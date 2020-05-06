08d44efb96fffbacc5b09cb62e94877b4a6c3985
kunjandhoble <kunjand31@gmail.com>
2020-05-06 07:53:38 -0500

    changes done

--- rahul_test.py
+++ rahul_test.py
@@ -1,17 +1,11 @@
 import pandas as pd
+import json
 
-# tc_data = pq.read_table(source='teachers.parquet').to_pandas()
+
 tc_data = pd.read_parquet('teachers.parquet', engine='pyarrow')
 st_data = pd.read_csv('students.csv')
 
-new_st_data = st_data[st_data.columns[0]].str.split("_", n = 6, expand=True)
-# st_data['cid'] = st_data['id_fname_lname_email_ssn_address_cid'].map(lambda x:x.split('_')[-1])
-
-# print(st_data)
-# st_data['fname'] = st_data['id_fname_lname_email_ssn_address_cid'].map(lambda x:x.split('_')[1])
-# st_data['lname'] = st_data['id_fname_lname_email_ssn_address_cid'].map(lambda x:x.split('_')[1])
-#
-#
+new_st_data = st_data[st_data.columns[0]].str.split("_", n=6, expand=True)
 new_st_data['name'] = new_st_data[1] + '_' + new_st_data[2]
 new_st_data['cid'] = new_st_data[6]
 
@@ -34,10 +28,6 @@ for index, row in new_st_data.iterrows():
                     "teacher_id": tc_row['id']
                 })
 
-import json
-with open('json_data.json', 'w') as fp:
-    json.dump(json_data, fp)
-
-
-
-
+with open('report.json', 'w') as fp:
+    report = {"Report_Data": json_data}
+    json.dump(report, fp)
